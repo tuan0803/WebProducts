6ef20cfa636da6d2e5b9231e894d4bfa1c67221f
tuan0803 <nvt08032001@gmail.com>
2024-12-16 16:37:11 +0700

    ORM

--- src/configs/db.js
+++ src/configs/db.js
@@ -8,13 +8,7 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD, 
   {
     host: process.env.DB_HOST, 
-    dialect: 'mysql',          
-    pool: {
-      max: 10,                 
-      min: 0,                  
-      acquire: 30000,         
-      idle: 10000             
-    },
+    dialect: 'mysql',
     logging: false,            
   }
 );
